4f83e3c07d461f74ab8ece52de5cb276f58e3573
Lorenz Müller <lo.mueller@edu.uni-graz.at>
2026-01-10 14:41:56 +0100

    first task completed

---
+++ function.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+import os
+
+def import_pos_file(filepath):
+    df = pd.read_csv(
+        filepath,
+        comment="#",
+        sep=r'\s+',
+        header=None,
+        names=["system", "satellite", "date", "time", "X", "Y", "Z"]
+    )
+
+    df["datetime"] = pd.to_datetime(df["date"] + " " + df["time"])
+    df = df.drop(columns=["date", "time"])
+
+    return df
+
+def plot_orbits(ecef, ecsf, satellite_id = None, r=6371000):
+
+    # Kugelparameter
+    u = np.linspace(0, 2*np.pi, 100)
+    v = np.linspace(0, np.pi, 100)
+    # Erde
+    x = r * np.outer(np.cos(u),np.sin(v)) # Vektorprodukt
+    y = r * np.outer(np.sin(u), np.sin(v))
+    z = r * np.outer(np.ones_like(u), np.cos(v))
+
+    # falls id einzelner PRN gegeben is
+    if satellite_id is not None:
+        ecef = ecef[ecef["satellite"] == satellite_id]
+        ecsf = ecsf[ecsf["satellite"] == satellite_id]
+
+    # Satellitenbahne [ECEF]
+    x_sat1 = ecef["X"].values
+    y_sat1 = ecef["Y"].values
+    z_sat1 = ecef["Z"].values
+    # Satellitenbahnen [ECSF]
+    x_sat2 = ecsf["X"].values
+    y_sat2 = ecsf["Y"].values
+    z_sat2 = ecsf["Z"].values
+
+    # Plotting
+    fig = plt.figure(figsize=(14, 8))
+    # ECEF
+    ax1 = fig.add_subplot(1,2,1, projection="3d")
+    ax1.plot_surface(x, y, z, color="lightblue", alpha=0.4,)
+    ax1.plot(x_sat1, y_sat1, z_sat1, color="red", linewidth=2, label="Satellite-Orbits")
+    ax1.set_xlabel("X [m]")
+    ax1.set_ylabel("Y [m]")
+    ax1.set_zlabel("Z [m]")
+    ax1.set_title("GNSS Satellite Orbit in ECEF")
+    ax1.set_box_aspect([1, 1, 1])
+    #ECSF
+    ax2 = fig.add_subplot(1,2,2, projection= "3d")
+    ax2.plot_surface(x, y, z, color="lightblue", alpha=0.4,)
+    ax2.plot(x_sat2, y_sat2, z_sat2, color="blue", linewidth=2, label="Satellite-Orbits")
+    ax2.set_xlabel("X [m]")
+    ax2.set_ylabel("Y [m]")
+    ax2.set_zlabel("Z [m]")
+    ax2.set_title("GNSS Satellite Orbit in ECSF")
+    ax2.set_box_aspect([1, 1, 1])
+
+    plt.tight_layout()                   
+    plt.show()
+
+def cart_to_geodetic(X,Y,Z):
+
+    # Parameter von WGS-84
+    a = 6378137.0 # große Halbachse
+    f = 1 / 298.257223563 # Abplattung
+    b = a * (1 - f) #kleine Halbachse
+    e2  = (a**2 - b**2) / a**2   # erste Exzentrizität^2
+    ep2 = (a**2 - b**2) / b**2   # zweite Exzentrizität^2
+
+    p = np.sqrt(X**2 + Y**2)
+    theta = np.arctan2(Z*a, p*b)
+
+    lon = np.arctan2(Y, X)
+    lat = np.arctan2(Z+ep2*b*np.sin(theta)**3, p - e2 * a * np.cos(theta)**3)
+
+    N = a / np.sqrt(1 - e2 * np.sin(lat)**2)
+    h = p/np.cos(lat) - N
+
+    return np.degrees(lat), np.degrees(lon), h
---
+++ main.py
@@ -0,0 +1,66 @@
+import os
+import matplotlib.pyplot as plt
+from function import import_pos_file, plot_orbits, cart_to_geodetic
+import numpy as np
+import pandas as pd
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+# Arbeitsumgebung und Import der Daten
+dir = os.getcwd()
+data = os.path.join(dir, "Daten")
+ecef = import_pos_file(data + "/pos_ECEF.txt")
+print(ecef.head())
+ecsf = import_pos_file(data + "/pos_ECSF.txt")
+print(ecsf.head())
+
+# Erdparameter
+r = 6371000 # Erdradius in Metern
+u = np.linspace(0, 2*np.pi, 100)
+v = np.linspace(0, np.pi, 100)
+# Erde
+x = r * np.outer(np.cos(u),np.sin(v)) # Vektorprodukt
+y = r * np.outer(np.sin(u), np.sin(v))
+z = r * np.outer(np.ones_like(u), np.cos(v))
+
+print(ecef.head())
+print(ecsf.head())
+
+# Plotted Orbits
+plot_orbits(ecef, ecsf)
+
+# Groundplot zweier Satelliten
+# Auswahl bestimmter PRN
+prn1 = 4
+prn2 = 16
+satellite_1 = ecef[ecef["satellite"] == prn1]
+satellite_2 = ecef[ecef["satellite"] == prn2] 
+
+lat1, lon1, h1 = cart_to_geodetic(satellite_1["X"].values, satellite_1["Y"].values, satellite_1["Z"].values)
+lat2, lon2, h2 = cart_to_geodetic(satellite_2["X"].values, satellite_2["Y"].values, satellite_2["Z"].values)
+
+
+# Entfernt Linien bei Sprüngen von über 360°
+lon1 = np.unwrap(np.radians(lon1))
+lon1 = np.degrees(lon1)
+lon2 = np.unwrap(np.radians(lon2))
+lon2 = np.degrees(lon2)
+
+# Eigentlicher Groundplot
+fig = plt.figure(figsize=(12, 6))
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()
+gl = ax.gridlines(draw_labels=True)
+gl.top_labels = False   # Oben keine Labels
+gl.left_labels = False  # Links keine Labels
+
+ax.plot(lon1, lat1, transform=ccrs.PlateCarree(),
+        label=f"PRN {prn1}")
+
+ax.plot(lon2, lat2, transform=ccrs.PlateCarree(),
+        label=f"PRN {prn2}")
+
+ax.set_title(f"Groundtrackplot von PRN {prn1} & {prn2}")
+ax.legend()
+
+plt.show()
